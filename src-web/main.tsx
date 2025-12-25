aff3a2933eea99d77de496905ff3bdbab92849a1
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-12-25 11:35:12 -0800

    feat: add react-scan

--- src-web/main.tsx
+++ src-web/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
+import { scan } from "react-scan";
+
 import { ThemeProvider } from "~/providers/theme";
 import { DragProvider } from "~/providers/drag-context";
 
 import { routeTree } from "~/routeTree.gen";
 
 const router = createRouter({ routeTree });
+
+scan({
+  enabled: import.meta.env.DEV,
+});
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
