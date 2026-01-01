d0af03c753de4e04202b2c21d240d9de8e6cbb94
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-12-31 17:37:16 -0800

    remove highlight color

--- src-web/features/editor/editor-theme.ts
+++ src-web/features/editor/editor-theme.ts
@@ -2,8 +2,11 @@ import { EditorView } from '@codemirror/view';
 
 export const editorTheme = EditorView.theme({
   '&': {
+    height: '100%',
     fontSize: '16px',
     fontFamily: 'ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Inter", "Helvetica Neue", sans-serif',
+    backgroundColor: 'var(--background)',
+    color: 'var(--foreground)',
   },
   '.cm-scroller': {
     fontFamily: 'inherit',
@@ -27,52 +30,13 @@ export const editorTheme = EditorView.theme({
   '&.cm-focused': {
     outline: 'none',
   },
-  '.cm-selectionBackground, .cm-content ::selection': {
-    backgroundColor: 'var(--highlight) !important',
-  },
-  '&.cm-focused .cm-selectionBackground': {
-    backgroundColor: 'var(--highlight) !important',
-  },
+  // '.cm-selectionBackground, .cm-content ::selection': {
+  //   backgroundColor: 'var(--highlight) !important',
+  // },
+  // '&.cm-focused .cm-selectionBackground': {
+  //   backgroundColor: 'var(--highlight) !important',
+  // },
   '.cm-activeLine': {
     backgroundColor: 'transparent',
   },
-}, { dark: false });
-
-export const editorThemeDark = EditorView.theme({
-  '&': {
-    height: '100%',
-    fontSize: '16px',
-    fontFamily: 'ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Inter", "Helvetica Neue", sans-serif',
-    backgroundColor: 'var(--background)',
-    color: 'var(--foreground)',
-  },
-  '.cm-scroller': {
-    fontFamily: 'inherit',
-    overflow: 'auto',
-  },
-  '.cm-content': {
-    fontFamily: 'inherit',
-    padding: '3rem',
-    lineHeight: '24px',
-    caretColor: 'var(--foreground)',
-  },
-
-  '.cm-gutters': {
-    display: 'none',
-  },
-  '.cm-line': {
-    padding: '0',
-  },
-  '&.cm-focused': {
-    outline: 'none',
-  },
-  '.cm-selectionBackground, .cm-content ::selection': {
-    backgroundColor: 'var(--highlight) !important',
-  },
-  '&.cm-focused .cm-selectionBackground': {
-    backgroundColor: 'var(--highlight) !important',
-  },
-  '.cm-activeLine': {
-    backgroundColor: 'transparent',
-  },
-}, { dark: true });
+});
